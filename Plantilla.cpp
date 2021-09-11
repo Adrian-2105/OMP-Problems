1ee05bfc0c4fa619c881e5cd8fc9f3d3a8d353c8
Adrián Fenollar Navarro <adrianfenollarnavarro@gmail.com>
2021-09-11 18:06:06 +0200

    Actualizada plantilla

--- Plantilla.cpp
+++ Plantilla.cpp
@@ -3,53 +3,72 @@ using namespace std;
 
 #include <iostream>
 #include <stdio.h>
-#include <algorithm>  
+#include <algorithm>
 #include <assert.h>
 #include <bits/stdc++.h>
 #include <bitset>
-#include <cmath>  
-#include <complex>  
-#include <deque>  
+#include <cmath>
+#include <complex>
+#include <deque>
 #include <functional>
-#include <limits.h>  
-#include <map>  
-#include <math.h>  
-#include <queue>  
-#include <set>  
+#include <limits.h>
+#include <map>
+#include <math.h>
+#include <queue>
+#include <set>
 #include <stack>
-#include <stdlib.h>  
-#include <string.h>  
-#include <string>  
-#include <time.h>  
-#include <unordered_map>  
+#include <stdlib.h>
+#include <string.h>
+#include <string>
+#include <time.h>
+#include <unordered_map>
 #include <unordered_set>
 #include <numeric>
 #include <vector>
 
-#define maxn 501
-#define mod 1000009
-#define infinite 100000000
+#define INF 200000000
 #define FOR(i, j, k, in) for (int i = j; i < k; i += in)
 #define REP(i, j) FOR(i, 0, j, 1)
+// Abreviaciones tipos de datos
 typedef unsigned long long int ull;
 typedef long long int ll;
 typedef double db;
 typedef long double ldb;
 typedef pair<int, int> pii;
+typedef pair<int, pair<int, int>> piii;
 typedef pair<ll, ll> pll;
-typedef pair <ll, int> pli;
 typedef pair <db, db> pdd;
 typedef vector<int> vi;
 typedef vector< vector<int> > vvi;
 typedef vector<pair<int, int>> vii;
-#define mp make_pair  
-#define Fi first  
-#define Se second  
-#define pb(x) push_back(x)  
-#define szz(x) ((int)(x).size())  
-#define all(x) (x).begin(), (x).end()  
+typedef stringstream ss;
+
+#define mp make_pair
+#define Fi first
+#define Se second
+#define pb(x) push_back(x)
+#define ms(x, y) memset(x, y, sizeof(x))
+#define sz(x) ((int)(x).size())
+#define all(x) (x).begin(), (x).end()
 #define ldb ldouble
 
-int main() {
+// Funciones macro
+#define read_array(v, n) REP(i,n) cin >> v[i]
+#define read_vector(v, n, aux) REP(i,n) {cin >> aux; v.pb(aux);}
+#define read_matrix(M, m, n) REP(i,m) REP(j,n) cin >> M[i][j]
+#define read_line_SS(SS) string s; getline(cin, s); SS << s
+#define print_array(a, n) REP(i,n) cout << a[i] << " "; cout << endl
+#define print_vector(v) for (auto a : v) cout << a << " "; cout << endl
+#define print_matrix(M, m, n) REP(i, m) {REP(j, n) cout << M[i][j]; cout << endl;}
 
+void solve() {
+
+}
+
+int main() {
+    int casos;
+    cin >> casos;
+    while (casos--) {
+        solve();
+    }
 }
